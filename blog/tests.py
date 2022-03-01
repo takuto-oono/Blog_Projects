461fe8353a843d492b85d1d7b2a82e54f63289b3
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-03-01 16:07:18 +0900

    test empty

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Article
 
-# Create your tests here.
+
+class ArticleModelTests(TestCase):
+    def test_is_empty(self):
+        saved_articles = Article.objects.all()
+        self.assertEqual(saved_articles.count(), 0)
