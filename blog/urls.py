2e72a4dcbde49b8736582c77ee89115014ffeeaf
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-03-11 16:10:14 +0900

    add h1 tag in index.html

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('create_comment/<int:article_pk>',
          views.CreateCommentView.as_view(), name='create_comment'),
     path('do_good/<int:article_pk>', views.do_good, name='do_good'),
-    path('read_later/<int: article_pk>', views.read_later, name='read_later'),
+    path('read_later/<int:article_pk>', views.read_later, name='read_later'),
 ]
