e7f5aaf41cb0548957d545e301d7f4d8bd8f3b42
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-03-18 10:22:34 +0900

    add url adminadmin

--- blog/urls.py
+++ blog/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
                   path('do_good/<int:article_pk>', views.do_good, name='do_good'),
                   path('read_later/<int:article_pk>', views.read_later, name='read_later'),
                   path('category/<int:category_pk>', views.CategoryDetail.as_view(), name='category'),
+                  path('admin_admin', views.AdminArticleList.as_view(), name='admin_article_list'),
                   path('admin_admin/create_article', views.CreateArticle.as_view(), name='create_article'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
