8f2157844799a7ba965ac6aa109f40d09b68ef95
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-03-28 10:53:14 +0900

    upgrade delete comment url adding article_pk

--- blog/urls.py
+++ blog/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('create_comment/<int:article_pk>',
         views.CreateCommentView.as_view(), name='create_comment'),
     path('edit/commet/<int:pk>/<int:article_pk>/',
-        views.EditComment.as_view(), name='edit_comment')
-    path('delete/comment/<int:pk>', views.DeleteComment.as_view(), name='delete_comment'),
+        views.EditComment.as_view(), name='edit_comment'),
+    path('delete/comment/<int:pk>/<int:article_pk>', views.DeleteComment.as_view(), name='delete_comment'),
     path('do_good/<int:article_pk>', views.do_good, name='do_good'),
     path('read_later/<int:article_pk>', views.read_later, name='read_later'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
